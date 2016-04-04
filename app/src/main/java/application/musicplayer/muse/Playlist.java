9eb6c87584e9a74b940ef9710150902f7c85cea2
bvisch <bvisch@gmail.com>
2016-04-04 13:14:56 -0400

    Added playlist class

---
+++ app/src/main/java/application/musicplayer/muse/Playlist.java
@@ -0,0 +1,24 @@
+package application.musicplayer.muse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ben on 4/4/2016.
+ */
+public class Playlist {
+    public List<Song> songList;
+
+    public Playlist() {
+        songList = new ArrayList<Song>();
+    }
+
+    public void add(Song s) {
+        songList.add(s);
+    }
+
+    public Playlist merge(Playlist pl) {
+        this.songList.addAll(pl.songList);
+        return this;
+    }
+}
